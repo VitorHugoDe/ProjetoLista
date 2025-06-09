520eb5337cd51a2daca4af671f4894209e7d0b4d
Vitor Hugo <garotao102009@gmail.com>
2025-06-09 06:46:23 -0300

    Criação dos filtros

---
+++ ListWeb/Models/Filtros.cs
@@ -0,0 +1,35 @@
+﻿namespace ListWeb.Models
+{
+    public class Filtros
+    {
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string StatusId { get; set; }
+        public string Vencimento { get; set; }
+
+        public Filtros(string filtroString)
+        {
+            FiltroString = filtroString ?? "todos-todos-todos";
+            string[] filtros = FiltroString.Split('-');
+            CategoriaId = filtros[0];
+            StatusId = filtros[1];
+            Vencimento = filtros[2];
+        }
+
+        public bool TemCategoria => CategoriaId.ToLower() != "todos";
+        public bool TemVencimento => Vencimento.ToLower() != "todos";
+        public bool Temstatus => StatusId.ToLower() != "todos";
+
+        public static Dictionary<string, string> VencimentoValoresFiltro =>
+            new Dictionary<string, string>
+            {
+                {"futuro", "Futuro"},
+                {"passado", "Passadp"},
+                {"hoje", "Hoje"}
+            };
+
+        public bool EPassado => Vencimento.ToLower() == "passado";
+        public bool EFuturo => Vencimento.ToLower() == "futuro";
+        public bool EHoje => Vencimento.ToLower() == "hoje";
+    }
+}
